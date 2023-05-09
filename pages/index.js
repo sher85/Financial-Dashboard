88e9519a8d422c3c8b0794bdcf32b88bfd556557
sher85 <32082005+sher85@users.noreply.github.com>
2023-05-08 20:11:55 -0500

    fixed issue with styling, i think it was the missing postcss.config.js file

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
-
-import Header from '../components/Header';
+import Header from '@/components/Header';
 
 export default function Home() {
 	return (
