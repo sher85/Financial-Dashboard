1255c3dc4338fa1365b6a0bbe0d4020f10d351d2
sher85 <32082005+sher85@users.noreply.github.com>
2023-05-12 10:49:31 -0500

    topcards with dummy data complete

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,7 +1,49 @@
 import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { AiOutlineHome } from 'react-icons/ai';
+import { RxDashboard, RxPerson } from 'react-icons/rx';
+import { HiOutlineShoppingBag } from 'react-icons/hi';
+import { FiSettings } from 'react-icons/fi';
 
-const Sidebar = () => {
-	return <div>Sidebar</div>;
+const Sidebar = ({ children }) => {
+	return (
+		<div className="flex">
+			{/* Sidebar */}
+			<div className="fixed w-20 h-screen p-4 bg-white border-r-[1px] flex flex-col justify-between">
+				<div className="flex flex-col items-center">
+					<Link href="/">
+						<div className="bg-purple-800 text-white p-3 rounded-lg inline-block">
+							<AiOutlineHome size={20} />
+						</div>
+					</Link>
+					<span className="border-b-[1.5px] border-gray-100 w-full p-2"></span>
+					<Link href="/customers">
+						<div className="bg-gray-200 hover:bg-gray-300 cursor-pointer my-4 p-3 rounded-lg inline-block">
+							<RxDashboard size={20} />
+						</div>
+					</Link>
+					<Link href="/person">
+						<div className="bg-gray-200 hover:bg-gray-300 cursor-pointer my-4 p-3 rounded-lg inline-block">
+							<RxPerson size={20} />
+						</div>
+					</Link>
+					<Link href="/orders">
+						<div className="bg-gray-200 hover:bg-gray-300 cursor-pointer my-4 p-3 rounded-lg inline-block">
+							<HiOutlineShoppingBag size={20} />
+						</div>
+					</Link>
+					<Link href="/settings">
+						<div className="bg-gray-200 hover:bg-gray-300 cursor-pointer my-4 p-3 rounded-lg inline-block">
+							<FiSettings size={20} />
+						</div>
+					</Link>
+				</div>
+			</div>
+			{/* Main Page */}
+			<main className="ml-20 w-full">{children}</main>
+		</div>
+	);
 };
 
 export default Sidebar;
---
+++ components/TopCards.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const TopCards = () => {
+	return (
+		<div className="grid lg:grid-cols-5 gap-4 p-4">
+			<div className="lg:col-span-2 col-span-1 bg-white flex justify-between w-full border p-4 rounded-lg">
+				<div className="flex flex-col pb-4">
+					<p className="text-2xl font-bold">$8,241</p>
+					<p className="text-gray-600">Daily Revenue</p>
+				</div>
+				<p className="bg-green-200 flex justify-center items-center p-2 rounded-lg">
+					<span className="text-green-700 text-lg">+18%</span>
+				</p>
+			</div>
+			<div className="lg:col-span-2 col-span-1 bg-white flex justify-between w-full border p-4 rounded-lg">
+				<div className="flex flex-col pb-4">
+					<p className="text-2xl font-bold">$1,637,846</p>
+					<p className="text-gray-600">YTD</p>
+				</div>
+				<p className="bg-green-200 flex justify-center items-center p-2 rounded-lg">
+					<span className="text-green-700 text-lg">+13%</span>
+				</p>
+			</div>
+			<div className="col-span-1 bg-white flex justify-between w-full border p-4 rounded-lg">
+				<div className="flex flex-col pb-4">
+					<p className="text-2xl font-bold">7,937</p>
+					<p className="text-gray-600">Customers</p>
+				</div>
+				<p className="bg-green-200 flex justify-center items-center p-2 rounded-lg">
+					<span className="text-green-700 text-lg">+8%</span>
+				</p>
+			</div>
+		</div>
+	);
+};
+
+export default TopCards;
